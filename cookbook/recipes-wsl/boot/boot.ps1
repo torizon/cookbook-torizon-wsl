400fb316ff3f26e384120a55a9a1658c2264790b
Matheus Castello <matheus@castello.eng.br>
2024-03-26 01:53:24 -0300

    recipes-wsl: add boot recipe
    THe WSL 2 has a boot section where we can add scripts to be executed
    when the WSL is started. However, this section does not work on Windows
    10 and below. So, we need to create a script that will be executed
    when the WSL is started.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ cookbook/recipes-wsl/boot/boot.ps1
@@ -0,0 +1,60 @@
+#!/usr/bin/env pwsh
+
+##
+# Bootstrapping the Windows Subsystem for Linux (WSL)
+# We use this script instead the [boot] section because
+# it does not work on Windows 10 and below.
+##
+
+# check if there already another user setup than root
+# get all regular users
+function Get-User {
+    $user = $null
+    $user = Get-Content /etc/passwd | ForEach-Object {
+        $fields = $_.Split(':')
+        if ($fields[2] -eq 1000) {
+            $fields[0]
+        }
+    }
+
+    return $user
+}
+
+$_user = Get-User
+
+if ($null -eq $_user) {
+    $env:WAYLAND_DISPLAY=$null
+    # no users found, create a new user
+    Set-Location /usr/welcome
+    ./torizonWSLWelcome
+
+    $_user = Get-User
+    if ($null -ne $_user) {
+        # add user to sudo group
+        usermod -aG sudo $_user
+
+        # docker config
+        usermod -aG docker $_user
+
+        # create the .bashrc
+        cp /etc/bashrc.template /home/$_user/.bashrc
+
+        # and pass the ownership of the home directory to the user
+        chown -R ${_user}:$_user /home/$_user
+    } else {
+        Write-Host -ForegroundColor Red "Error: User not added?"
+        exit 69
+    }
+}
+
+# start docker service ??
+/usr/sbin/service docker status
+if ($LASTEXITCODE -ne 0) {
+    /usr/sbin/service docker start
+}
+
+# change the user
+Set-Location /home/$_user
+sudo -u $_user /bin/bash --noprofile -i
+
+exit 0
---
+++ cookbook/recipes-wsl/boot/deploy.ts
@@ -0,0 +1,75 @@
+#!/opt/bun/bin/bun
+
+import PATH from "path"
+import FS from "fs"
+import logger from "node-color-log"
+import { execSync } from "child_process"
+
+const ARCH = process.env.ARCH as string
+const MACHINE = process.env.MACHINE as string
+const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
+const BUILD_PATH = process.env.BUILD_PATH as string
+const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
+const DISTRO_MINOR = process.env.DISTRO_MINOR as string
+const DISTRO_PATCH = process.env.DISTRO_PATCH as string
+const USER_PASSWD = process.env.USER_PASSWD as string
+
+// get the actual script path, not the process.cwd
+const _path = PATH.dirname(process.argv[1])
+const meta = JSON.parse(process.env.META as string)
+
+logger.info(`deploy ${meta.name} ...`)
+
+const IMAGE_MNT_BOOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/boot`
+const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
+process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
+process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
+
+// copy the boot script to the rootfs
+execSync(
+    `echo ${USER_PASSWD} | sudo -E -S ` +
+    `cp ${_path}/boot.ps1 ${IMAGE_MNT_ROOT}/bin/`,
+    {
+        shell: "/bin/bash",
+        stdio: "inherit",
+        encoding: "utf-8",
+        env: process.env
+    })
+
+// give the boot script execution permission
+execSync(
+    `echo ${USER_PASSWD} | sudo -E -S ` +
+    `chmod +x ${IMAGE_MNT_ROOT}/bin/boot.ps1`,
+    {
+        shell: "/bin/bash",
+        stdio: "inherit",
+        encoding: "utf-8",
+        env: process.env
+    })
+
+// add the boot to the /etc/profile if not already there
+const profile = FS.readFileSync(`${IMAGE_MNT_ROOT}/etc/profile`, "utf-8")
+if (!profile.includes("boot.ps1")) {
+    execSync(
+        `echo ${USER_PASSWD} | sudo -E -S ` +
+        `bash -c "echo 'boot.ps1' >> ${IMAGE_MNT_ROOT}/etc/profile"`,
+        {
+            shell: "/bin/bash",
+            stdio: "inherit",
+            encoding: "utf-8",
+            env: process.env
+        })
+}
+
+if (!profile.includes("exit")) {
+    execSync(
+        `echo ${USER_PASSWD} | sudo -E -S ` +
+        `bash -c "echo 'exit' >> ${IMAGE_MNT_ROOT}/etc/profile"`,
+        {
+            shell: "/bin/bash",
+            stdio: "inherit",
+            encoding: "utf-8",
+            env: process.env
+        })
+}
+logger.success(`Deployed ${meta.name}!`)
