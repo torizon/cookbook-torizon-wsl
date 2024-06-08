a6b0ce66390df20075c6c28087a5fa05d9e25f08
Matheus Castello <matheus@castello.eng.br>
2024-06-07 22:34:13 -0300

    simple-update: Add recipe
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ cookbook/recipes-wsl/simple-update/deploy.ts
@@ -0,0 +1,60 @@
+#!/opt/bun/bin/bun
+
+import PATH from "path"
+import FS from "fs"
+import logger from "node-color-log"
+import { execSync } from "child_process"
+
+const ARCH = process.env.ARCH as string
+const MACHINE = process.env.MACHINE as string
+const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
+const BUILD_PATH = process.env.BUILD_PATH as string
+const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
+const DISTRO_MINOR = process.env.DISTRO_MINOR as string
+const DISTRO_PATCH = process.env.DISTRO_PATCH as string
+const USER_PASSWD = process.env.USER_PASSWD as string
+
+// get the actual script path, not the process.cwd
+const _path = PATH.dirname(process.argv[1])
+const meta = JSON.parse(process.env.META as string)
+
+logger.info(`deploy ${meta.name} ...`)
+
+const IMAGE_MNT_BOOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/boot`
+const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
+process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
+process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
+
+// create the folder just in case
+execSync(
+    `echo ${USER_PASSWD} | sudo -E -S ` +
+    `mkdir -p ${IMAGE_MNT_ROOT}/opt/updater`,
+    {
+        shell: "/bin/bash",
+        stdio: "inherit",
+        encoding: "utf-8",
+        env: process.env
+    })
+
+// copy the boot script to the rootfs
+execSync(
+    `echo ${USER_PASSWD} | sudo -E -S ` +
+    `cp ${_path}/boot.ps1 ${IMAGE_MNT_ROOT}/opt/updater/`,
+    {
+        shell: "/bin/bash",
+        stdio: "inherit",
+        encoding: "utf-8",
+        env: process.env
+    })
+
+// give the boot script execution permission
+execSync(
+    `echo ${USER_PASSWD} | sudo -E -S ` +
+    `chmod +x ${IMAGE_MNT_ROOT}/opt/updater/updater.ps1`,
+    {
+        shell: "/bin/bash",
+        stdio: "inherit",
+        encoding: "utf-8",
+        env: process.env
+    })
+logger.success(`Deployed ${meta.name}!`)
---
+++ cookbook/recipes-wsl/simple-update/updater.ps1
@@ -0,0 +1,37 @@
+#!/usr/bin/env pwsh
+
+##
+# Check if there is a new version
+# then deploy the new files
+##
+
+$version = "v0.0.9-rc6"
+
+try {
+    # 1. Check if there is a new version
+    $latestData = Invoke-RestMethod -Uri `
+        "https://api.github.com/repos/commontorizon/cookbook-torizon-wsl/releases"
+
+    if ($version -eq $latestData[0].tag_name) {
+        Write-Host "No new version available"
+        exit 0
+    }
+
+    # 2. Get the list of files to download
+    $reqFiles = Invoke-WebRequest -Uri `
+        "https://raw.githubusercontent.com/commontorizon/cookbook-torizon-wsl/main/cookbook/recipes-wsl/simple-update/files.json"
+    $files = $reqFiles.Content | ConvertFrom-Json
+
+    # 3. Download the files and deploy them
+    foreach ($file in $files.update) {
+        Write-Host "Downloading :: $($file.file)"
+        Invoke-WebRequest -Uri $file.file -OutFile $file.deploy
+
+        if ($file.exec -eq $true) {
+            chmod +x $file.deploy
+        }
+    }
+}
+catch {
+    Write-Host "An error occurred: $_"
+}
